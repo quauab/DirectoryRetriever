2bc877d39276eed3d16055ed0434f9ae253cbab8
quauab <ichglauben@gmail.com>
2016-05-04 22:41:54 -0400

    Minor update

--- directoryretriever/src/main/java/com/gmail/ichglauben/directoryretriever/App.java
+++ directoryretriever/src/main/java/com/gmail/ichglauben/directoryretriever/App.java
@@ -6,6 +6,10 @@ import com.gmail.ichglauben.directoryretriever.core.utils.abstracts.CustomClass;
 public class App extends CustomClass {
 	public static void main(String[] args) {
 //		testRetrieveDirectoryNoArgs();
+//		testRetrieveDirectoryFilter();
+	}
+
+	private static void testRetrieveDirectoryFilter() {
 		String dir = DirectoryRetriever.retrieveDirectory("Jpegs", new String[] {"jpg"});
 		print("Chosen directory: " + dir);
 	}

875e12fdad3a9c7bbb06a747b60452b54bd536b3
quauab <ichglauben@gmail.com>
2016-05-04 22:40:49 -0400

    Added experimental overloaded filtered retrieveDirectory method. String description & String[] list of extensions

--- directoryretriever/src/main/java/com/gmail/ichglauben/directoryretriever/App.java
+++ directoryretriever/src/main/java/com/gmail/ichglauben/directoryretriever/App.java
@@ -5,6 +5,12 @@ import com.gmail.ichglauben.directoryretriever.core.utils.abstracts.CustomClass;
 
 public class App extends CustomClass {
 	public static void main(String[] args) {
+//		testRetrieveDirectoryNoArgs();
+		String dir = DirectoryRetriever.retrieveDirectory("Jpegs", new String[] {"jpg"});
+		print("Chosen directory: " + dir);
+	}
+
+	private static void testRetrieveDirectoryNoArgs() {
 		String dir = DirectoryRetriever.retrieveDirectory();
 		print("Chosen directory: " + dir);
 	}
